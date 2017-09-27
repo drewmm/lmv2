74ecfb873e97f418c9fd7e235582747d1998d994
Drew Messinger-Michaels <drewmmichaels@gmail.com>
2017-09-26 23:02:33 -0500

    again

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php
+// ****** Set Your Email Address For Mail Forms ******************
+$From_Email = "From: me@laurenvillegas.com";
+$Admin_Email = "me@laurenvillegas.com";
+?>
--- src/main/webapp/include/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-// ****** Set Your Email Address For Mail Forms ******************
-$From_Email = "From: me@laurenvillegas.com";
-$Admin_Email = "me@laurenvillegas.com";
-?>
