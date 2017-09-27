8065fa559787df03d9ccb5dc1ff74f1a3f093dfb
Drew Messinger-Michaels <drewmmichaels@gmail.com>
2017-09-26 23:40:53 -0500

    for gitpages

---
+++ old_phps/media.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<meta name="description" content="Lauren Villegas &mdash; Actor, Singer, Performer">
+<meta name="keywords" content="singer, actor, actresses, mezzo">
+<link rel="shortcut icon" href="favicon.ico"/>
+<title>Lauren Villegas &mdash; Actor, Singer, Performer</title>
+
+<link rel="stylesheet" href="css/bootstrap-1.0.0.css" />
+<link rel="stylesheet" href="css/960_12_col.css" />
+<link rel="stylesheet" href="css/chosen.css" />
+
+<!--[if lt IE 9]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+<![endif]-->
+<script src="js/libs/modernizr-2.0.6.min.js"></script>
+
+</head>
+<body>
+
+
+  <div id="header">
+	<div class="container_12">
+		<p></p>		  
+	</div>
+	<!-- end .container_12 -->
+  </div>
+  <div class="clear"></div>
+
+<body>
+  <div id="header">
+	<div class="container_12">
+		    <a href="index.php"><img src="img/lv.jpg"></a>
+	</div>
+	<!-- end .container_12 -->
+  </div>
+  <div class="clear"></div>
+
+
+
+
+  <div  id="main" class="container_12">
+  	<div class="grid_12 feature"><br/>
+<!-- Start VisualLightBox.com HEAD section -->
+		<link rel="stylesheet" href="index_files/vlb_files1/vlightbox1.css" type="text/css" />
+		<link rel="stylesheet" href="index_files/vlb_files1/visuallightbox.css" type="text/css" media="screen" />
+<script src="index_files/vlb_engine/jquery.min.js" type="text/javascript"></script>
+		<script src="index_files/vlb_engine/visuallightbox.js" type="text/javascript"></script>
+		<!-- End VisualLightBox.com HEAD section -->
+
+	</head>
+	<body bgcolor="#ffffff">
+		<!-- Start VisualLightBox.com BODY section id=1 -->
+	<div id="vlightbox1">
+	<a class="vlightbox1" href="index_files/vlb_images1/hs_03.jpg" title="Lauren Villegas 1"><img src="index_files/vlb_thumbnails1/hs_03.jpg" alt="Lauren Villegas 1"/></a>
+<a class="vlightbox1" href="index_files/vlb_images1/hs_02.jpg" title="Lauren Villegas 2"><img src="index_files/vlb_thumbnails1/hs_02.jpg" alt="Lauren Villegas 2"/></a>
+<a class="vlightbox1" href="index_files/vlb_images1/hs_01.jpg" title="Lauren Villegas 3"><img src="index_files/vlb_thumbnails1/hs_01.jpg" alt="Lauren Villegas 3"/></a>
+<a class="vlightbox1" href="index_files/vlb_images1/0img_7255.jpg" title="Lauren Villegas 4"><img src="index_files/vlb_thumbnails1/0img_7255.jpg" alt="Lauren Villegas 4"/></a>
+<a class="vlb" href="http://visuallightbox.com">simple jquery lightbox by VisualLightBox.com v5.4m</a>
+	</div>
+	<script src="index_files/vlb_engine/vlbdata1.js" type="text/javascript"></script>
+	<!-- End VisualLightBox.com BODY section --><br/>
+
+
+		</div>
+		</div>
+                        
+	
+  	</div>
+  	
+ <div id="main" class="container_12">
+  	<div class="grid_9 prefix_3">
+	  	<h2></h2>
+  	</div>
+  	<div class="clear"></div>	  	
+  	<div class="grid_3">
+  		<ul class="unstyled side-nav">
+  			<li><a href="index.php">News</a></li>
+  			<li><a href="resume.php">Resume</a></li>
+  			<li class="active">Media</li>
+  			<li><a href="contact.php">Contact</a></li>			  		<br/><br/>
+  		</ul>
+  	</div>
+  	<div class="grid_9">
+  		<object height="81" width="100%"><param name="movie" value="https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F84053325&amp;color=666666&amp;auto_play=false&amp;show_artwork=false&amp;show_playcount=false&amp;show_comments=false"></param><param name="allowscriptaccess" value="always"></param><embed allowscriptaccess="always" src="https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F84053325&amp;color=666666&amp;auto_play=false&amp;show_artwork=false&amp;show_playcount=false&amp;show_comments=false" type="application/x-shockwave-flash" width="100%" height="81"></embed></object><span><h4>"Ordinary Guy"<br/>Setting by Aaron Copland, Poem by Langston Hughes</h4><br/<br/>
+<object height="81" width="100%"><param name="movie" value="https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F84058540&amp;color=666666&amp;auto_play=false&amp;show_artwork=false&amp;show_playcount=false&amp;show_comments=false"></param><param name="allowscriptaccess" value="always"></param><embed allowscriptaccess="always" src="https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F84058540&amp;color=666666&amp;auto_play=false&amp;show_artwork=false&amp;show_playcount=false&amp;show_comments=false" type="application/x-shockwave-flash" width="100%" height="81"></embed></object><h4>"Rest Your Head and Sleep" from the musical CAMILLE by Julia Gregory<br/>Additional vocals by Christiane Noll</h4><br/<br/><br/>
+		</div>		
+  	</div>
+
+
+
+  <!-- END .container_12 --> 
+
+<div id="footer">
+	  <div>
+		<img src="img/say_my_name.jpg">
+		<br/><br/><br/>
+</div></div>
+
+
+</body>
+</html>
