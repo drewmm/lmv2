91a9923dce07ccdef219fc523120db971897ea02
Drew Messinger-Michaels <drewmmichaels@gmail.com>
2017-09-26 23:18:53 -0500

    blanking it out

--- index3_files/vlb_engine/vlbdata1.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox1',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:true,prefix:'vlb1',resizeSpeed:7,slideTime:4,startZoom:true}) });
